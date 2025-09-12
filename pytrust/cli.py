791ae6f02d0b51356e619b888017db739a01e062
Martin Thoma <info@martin-thoma.de>
2025-09-12 17:40:52 +0200

    Return required permissions if no permissions.yaml is given

--- pytrust/cli.py
+++ pytrust/cli.py
@@ -3,39 +3,44 @@ import yaml
 from .permissions import analyze_package, get_permission_violations, PermissionReport
 
 
+
 @click.command()
 @click.argument("package")
-@click.argument("permissions_file")
+@click.argument("permissions_file", required=False)
 @click.option("--verbose", is_flag=True, help="Print permissions.yaml content")
-def main(package, permissions_file, verbose):
+def main(package, permissions_file=None, verbose=False):
     """Check package permissions."""
-
-    with open(permissions_file, "r") as f:
-        permissions_dict = yaml.safe_load(f)
-    if not isinstance(permissions_dict, dict):
-        click.echo("permissions.yaml must be a dictionary with package names as keys.")
-        raise SystemExit(1)
 
     report = analyze_package(package)
 
-    pkg_perms = permissions_dict.get(package)
+    if permissions_file:
+        with open(permissions_file, "r") as f:
+            permissions_dict = yaml.safe_load(f)
+        if not isinstance(permissions_dict, dict):
+            click.echo("permissions.yaml must be a dictionary with package names as keys.")
+            raise SystemExit(1)
 
-    if verbose:
-        click.echo("Analysis result:")
-        for k, v in report.as_dict().items():
-            click.echo(f"{k}: {'Yes' if v else 'No'}")
+        pkg_perms = permissions_dict.get(package)
 
-    violations = get_permission_violations(
-        required_permissions=report, given_permissions=PermissionReport(**pkg_perms)
-    )
+        if verbose:
+            click.echo("Analysis result:")
+            for k, v in report.as_dict().items():
+                click.echo(f"{k}: {'Yes' if v else 'No'}")
 
-    if violations:
-        click.echo("Permission violations found:")
-        for key, required, given in violations:
-            click.echo(f" - {key}: REQUIRED but NOT GIVEN")
+        violations = get_permission_violations(
+            required_permissions=report, given_permissions=PermissionReport(**pkg_perms)
+        )
+
+        if violations:
+            click.echo("Permission violations found:")
+            for key, required, given in violations:
+                click.echo(f" - {key}: REQUIRED but NOT GIVEN")
+        else:
+            click.echo("No permission violations found.")
+            raise SystemExit(1)
     else:
-        click.echo("No permission violations found.")
-        raise SystemExit(1)
+        # No permissions_file: print valid YAML permission report
+        click.echo(yaml.dump({package: report.as_dict()}, sort_keys=False))
 
 
 if __name__ == "__main__":
